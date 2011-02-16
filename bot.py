c2eecc8569e734317da351af6326a543da6b51fd
Royan Feng <odayfans@gmail.com>
2011-02-17 01:53:03 +0800

    add support for multiple-server login

--- bot.py
+++ bot.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python2
 #-*-coding:utf-8-*-
-import socket
-server = 'irc.freenode.net'
-port = 6667
-nick = 'tyty'
-realname=nick
-channel='#ofan-bot,#ubuntu-cn'
+import socket,config
 
-irc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-irc.connect((server,port))
-irc.recv(4096)
-irc.send('NICK '+nick+'\r\n')
-irc.send('USER '+nick+' t t t :'+realname+'\r\n')
-irc.send('JOIN '+channel+'\r\n')
-irc.send('PRIVMSG '+channel+' :Hello.\r\n')
+# Sockets list
+Irc=[]
+# End Of Message
+EOM='\r\n'
+# Buffer size
+BuffSize=4096
+
+for serv in config.Networks:
+    if serv['server'][0]=='!':
+        continue
+    irc = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    irc.connect((serv['server'],serv['port']))
+    irc.recv(BuffSize)
+    irc.send('PASS '+serv['pass']+EOM)
+    irc.send('NICK '+serv['nick']+EOM)
+    irc.send('USER '+serv['nick']+' pizBot pizBot pizBot :'+serv['nick']+EOM)
+    irc.send('JOIN '+serv['channels']+EOM)
+    Irc.append((serv,irc))
+
 while True:
-    data = irc.recv(4096)
-    if data.find('PING')!=-1:
-        irc.send('PONG '+data.split()[1]+'\r\n')
-    elif data.find('PRIVMSG')!=-1:
-        source = data.split('!')[0].replace(':','')
-        message = ':'.join(data.split(':')[2:])
-        destination=''.join(data.split(':')[:2]).split(' ')[-2]
-        if destination==nick:
-            destination='PRIVATE'
-        print '(',destination,')',source+':',message
+    for profile,irc in Irc:
+        data = irc.recv(BuffSize)
+        print 'RAW '+data
+        if data.find('PING')!=-1:
+            print 'Ping from'+profile['short']
+            irc.send('PONG '+data.split()[1]+EOM)
+        elif data.find('PRIVMSG')!=-1:
+            source = data.split('!')[0].replace(':','')
+            message = ':'.join(data.replace('\r','').replace('\n','').split(':')[2:])
+            destination=''.join(data.split(':')[:2]).split(' ')[-2]
+            if destination==profile['nick']:
+                destination='PRIVATE'
+            print '(',destination,')',source+':',message
